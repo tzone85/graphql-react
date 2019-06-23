f8ab3ffee8db2e8d8b388bb51cdcfcafba68c135
tzone85 <tzone85@gmail.com>
2019-06-23 17:59:34 +0200

    added the author type to the schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 // dummy data
@@ -16,7 +17,13 @@ let books = [
     {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
     {name: 'Oaklahoma', genre: 'Fantasy', id: '2'},
     {name: 'Umzimba Wam', genre: 'Sci-Fi', id: '3'}
-]
+];
+
+let authors = [
+    {name: 'Patrick Mini', age: 40, id: 1},
+    {name: 'Mcedi Mini', age: 42, id: 2},
+    {name: 'Thando Sombolo', age: 36, id: 3}
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -24,6 +31,15 @@ const BookType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 });
 
@@ -37,10 +53,20 @@ const RouteQuery = new GraphQLObjectType({
                 // how we get data from db / other source
                 return _.find(books, {id: args.id});
             }
+        },
+
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                // find author based on the provided id
+                console.log('ndiyafika');
+                return _.find(authors, {id: args.id});
+            }
         }
     }
 });
 
 module.exports = new GraphQLSchema({
     query: RouteQuery
-})
+});
