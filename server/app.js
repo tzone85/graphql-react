ae0c853ed2ca52dc261c4b6e634d26cadff1de5f
tzone85 <tzone85@gmail.com>
2019-06-23 16:18:56 +0200

    installed graphql and started with the middleware with the route /graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+// used as middleware. Used as enpoint to interact with the graphQL data
+const graphqlHTTP = require('express-graphql');
+
 const app = express();
+
+// setting up the middleware
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(4000, () => {
     console.log('listenning to port 4000');
